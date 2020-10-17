66a39e7495b25e073657fad72ecac703f5204e03
N1kRolexx <Exrolexx21@gmail.com>
2020-10-17 12:01:00 +0200

    Arduino sketch.

--- arduino.py
+++ arduino.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import serial
 
@@ -6,8 +6,8 @@ import serial
 ser = serial.Serial()
 ser.baudrate = 115200           # should match arduino's sketch baudrate
 ser.port = '/dev/ttyAMA0'       # usual arduino port on Raspbian
-ser.timeout = 1                 # important for not to freeze the program, if some messages did not arrive
-ser.open()
+ser.timeout = 1                 # important for not to freeze the program, if some messages did not arrive,
+ser.open()                      # because arduino sends back info after every command
 
 def getInfo():
     ser.flushInput()
@@ -30,17 +30,3 @@ def sendSetter(setter, setterVal):
     ser.write(setter.encode() + "\n")
     reply = ser.readline()
     return reply
-
-# For testing purpose, when you have no arduino connected
-# def getInfo():
-#     sensors = "26;70".split(";")
-#     return sensors
-#
-# def sendCommand(command):
-#     reply = "received command: {}".format(command)
-#     return reply
-#
-# def sendSetter(setter, setterVal):
-#     reply = "setter: {} value: {}".format(setter, setterVal)
-#     return reply
-#
